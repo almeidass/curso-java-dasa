1fdf51433e3b7d1a512155f84964393acd659f12
Leonardo Almeida <l.almeida.ext@dasa.com.br>
2020-07-17 11:09:07 -0300

    Atualizar arquivo gitignore

--- Aula09_JDBC_Exercicio/src/main/java/br/com/dasa/gui/Contatos.java
+++ Aula09_JDBC_Exercicio/src/main/java/br/com/dasa/gui/Contatos.java
@@ -87,7 +87,7 @@ public class Contatos extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 562, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblTelefone)
@@ -98,7 +98,7 @@ public class Contatos extends javax.swing.JFrame {
                             .addComponent(txtNome)
                             .addComponent(txtEmail)
                             .addComponent(fTxtFone, javax.swing.GroupLayout.DEFAULT_SIZE, 226, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, 178, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btnCadastrar)))
                 .addContainerGap())
         );
